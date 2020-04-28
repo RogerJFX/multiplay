2556c7e58babf6ac1514c32056ac5591ee8c0de5
Roger <roger@crazything.de>
2020-04-28 20:20:39 +0200

    Refactoring arch

--- app/controllers/WebSocketCtrl.scala
+++ app/controllers/WebSocketCtrl.scala
@@ -1,31 +1,52 @@
 package controllers
 
-import akka.actor._
-import play.api.mvc._
-import play.api.libs.streams.ActorFlow
-import javax.inject.{Inject, Singleton}
-import akka.actor.ActorSystem
+import akka.actor.{Actor, ActorRef, ActorSystem, Props}
 import akka.stream.Materializer
+import entity.{InMsg, OutMsg}
+import javax.inject.{Inject, Singleton}
+import play.api.libs.json.{Json, OFormat}
+import play.api.libs.streams.ActorFlow
+import play.api.mvc._
+import play.api.mvc.WebSocket.MessageFlowTransformer
+import services.MyWebSocketActor
 
 @Singleton
 class WebSocketCtrl @Inject() (cc: ControllerComponents)(implicit system: ActorSystem, mat: Materializer)
   extends AbstractController(cc) {
 
-  println("HUASSXPIJASDPIHASFD")
-  def socket = WebSocket.accept[String, String] { _ =>
+  implicit val inEventFormat: OFormat[InMsg] = Json.format[InMsg]
+  implicit val outEventFormat: OFormat[OutMsg] = Json.format[OutMsg]
+  implicit val messageFlowTransformer: MessageFlowTransformer[InMsg, OutMsg] =
+    MessageFlowTransformer.jsonMessageFlowTransformer[InMsg, OutMsg]
+  def socket = WebSocket.accept[InMsg, OutMsg] { _ =>
     ActorFlow.actorRef { out =>
-      MyWebSocketActor.props(out)
+      // MyWebSocketActor.props(out)
+      Props(new MyWebSocketActor(out))
     }
   }
+
 }
 
-object MyWebSocketActor {
-  def props(out: ActorRef) = Props(new MyWebSocketActor(out))
-}
+//object MyWebSocketActor {
+//  def props(out: ActorRef) = Props(new MyWebSocketActor(out))
+//}
 
-class MyWebSocketActor(out: ActorRef) extends Actor {
-  def receive = {
-    case msg: String =>
-      out ! ("I received your message: " + msg)
-  }
-}
+//class MyWebSocketActor(out: ActorRef) extends Actor {
+//  override def receive = {
+//    case msg: InMsg =>
+//      println("receive")
+//      out ! OutMsg("I received your message: " + msg.msg)
+//    case a: Any =>
+//      println(a)
+//    case _ =>
+//      throw new RuntimeException("Not acceptable")
+//  }
+//
+//  override def postStop() = {
+//    println("closed")
+//  }
+//
+//  override def preStart(): Unit = {
+//    println("connect")
+//  }
+//}
--- build.sbt
+++ build.sbt
@@ -10,7 +10,9 @@ resolvers += "Akka Snapshot Repository" at "https://repo.akka.io/snapshots/"
 
 scalaVersion := "2.13.1"
 
-libraryDependencies ++= Seq( jdbc , ehcache , ws , specs2 % Test , guice )
+libraryDependencies += guice
+
+// libraryDependencies ++= Seq( jdbc , ehcache , ws , specs2 % Test , guice )
 
 //unmanagedResourceDirectories in Test <+=  baseDirectory ( _ /"target/web/public/test" )
 
